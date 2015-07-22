c65ea012042ed28d34c154d2f132574d968f367d
Bigjinbin <2213620570@qq.com>
2015-07-22 09:25:44 +0800

    Initial Commit

---
+++ MediaPlayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MediaPlayDemo
+//
+//  Created by jb on 15/7/22.
+//  Copyright (c) 2015年 jb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
